9ef4b7b3a96ce01d654bb1c01964ff1832e3140e
Evert <evertvandenbruel@gmail.com>
2022-12-02 19:45:54 +0100

    Day 1

---
+++ 01/aoc.rb
@@ -0,0 +1,14 @@
+current = 0
+results = []
+
+File.readlines('input').each do |line|
+	if Integer(line, exception: false)
+		current = current + line.to_i
+	else
+		results << current
+		current = 0
+	end
+end
+
+results << current
+puts results.sort.last(3).sum
