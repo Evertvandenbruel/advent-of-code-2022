78ebd7f50563a4618f6faff69ff9f20e13a73bdf
Evert <evertvandenbruel@gmail.com>
2022-12-02 20:34:00 +0100

    Day 2

--- 02/oacb.rb
+++ 02/oacb.rb
@@ -2,8 +2,6 @@
 # X for Rock, Y for Paper, and Z for Scissors.
 # X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win
 
-# SCORES = { "A X" => 4, "A Y" => 8, "A Z" => 3, "B X" => 1, "B Y" => 5, "B Z" => 9, "C X" => 7, "C Y" => 2, "C Z" => 6 }
-
 SCORES = { "A X" => 3, "A Y" => 4, "A Z" => 8, "B X" => 1, "B Y" => 5, "B Z" => 9, "C X" => 2, "C Y" => 6, "C Z" => 7 }
 
 score = 0
