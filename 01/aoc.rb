cdb8028ecf832569b60063886bf660a6d6eb3499
Evert <evertvandenbruel@gmail.com>
2022-12-02 19:46:38 +0100

    Using spaces instead of tabs

--- 01/aoc.rb
+++ 01/aoc.rb
@@ -2,12 +2,12 @@ current = 0
 results = []
 
 File.readlines('input').each do |line|
-	if Integer(line, exception: false)
-		current = current + line.to_i
-	else
-		results << current
-		current = 0
-	end
+  if Integer(line, exception: false)
+    current = current + line.to_i
+  else
+    results << current
+    current = 0
+  end
 end
 
 results << current
